4960ff642a6aa2af98c8088884290209efe911de
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-31 13:26:59 +0300

    Описывает функции, возвращающие разметку компонентов звания пользователя и меню

--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ const RenderPosition = {
   BEFOREEND: 'beforeend'
 };
 
+const createUserRankTemplate = () => {
+  return (
+    `<section class="header__profile profile">
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    </section>`
+  );
+};
+
+const createSiteMenuTemplate = () => {
+  return (
+    `<nav class="main-navigation">
+      <div class="main-navigation__items">
+        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+      </div>
+      <a href="#stats" class="main-navigation__additional">Stats</a>
+    </nav>`
+  );
+};
+
 /* В следующих заданиях добавятся другие места вставки,
 поэтому использованы перечисление и switch */
 const render = (container, component, place = RenderPosition.BEFOREEND) => {
