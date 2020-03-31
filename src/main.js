02707642b3319736e25375e08ad008cd1447d906
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-31 14:47:22 +0300

    Описывает функции, возвращающие разметку компонентов сортировки и списка фильмов

--- src/main.js
+++ src/main.js
@@ -28,6 +28,29 @@ const createSiteMenuTemplate = () => {
   );
 };
 
+const createSortTemplate = () => {
+  return (
+    `<ul class="sort">
+      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" class="sort__button">Sort by date</a></li>
+      <li><a href="#" class="sort__button">Sort by rating</a></li>
+    </ul>`
+  );
+};
+
+const createMovieListTemplate = () => {
+  return (
+    `<section class="films">
+      <section class="films-list">
+        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+
+        <div class="films-list__container">
+        </div>
+      </section>
+    </section>`
+  );
+};
+
 const createMovieCardTemplate = () => {
   return (
     `<article class="film-card">
