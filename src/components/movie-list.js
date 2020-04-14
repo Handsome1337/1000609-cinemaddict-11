75eced4a827ffab17fd0eb9fadbaf8dd0380b81d
Ilya Volkov <volkov-ilyha@ya.ru>
2020-04-14 16:52:25 +0300

    Модифицирует компонент MovieList на случай отсутствия фильмов

--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -1,25 +1,29 @@
 import {createElement} from './../utils.js';
 
-const createMovieListTemplate = () => {
+const createMovieListTemplate = (moviesCount) => {
+  const headlineMarkup = `<h2 class="films-list__title${moviesCount ? ` visually-hidden"> All movies. Upcoming` : `">There are no movies in our database`}</h2>`;
+  const movieListContainerMarkup = moviesCount ? `<div class="films-list__container">\n</div>` : ``;
+
   return (
     `<section class="films">
       <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+        ${headlineMarkup}
 
-        <div class="films-list__container">
-        </div>
+        ${movieListContainerMarkup}
       </section>
     </section>`
   );
 };
 
 export default class MovieList {
-  constructor() {
+  constructor(moviesCount) {
+    this._moviesCount = moviesCount;
+
     this._element = null;
   }
 
   getTemplate() {
-    return createMovieListTemplate();
+    return createMovieListTemplate(this._moviesCount);
   }
 
   getElement() {
